c12d95e42860fb0856350c9fcf951efcebf8e881
vikas <princechat2006@gmail.com>
2026-08-01 15:52:53 +0530

    Update CORS for Vercel frontend

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://resume-evaluator-two.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
